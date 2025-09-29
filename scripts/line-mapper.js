41de9536376070f924ecfca83b214160334493d7
Ricardo Dias <89875706+ricardodias06@users.noreply.github.com>
2025-09-29 17:46:52 +0100

    update (line 5) line-mapper.js

--- scripts/line-mapper.js
+++ scripts/line-mapper.js
@@ -1,6 +1,5 @@
 // line-mapper.js
-// Aqui defines manualmente que paragens pertencem a cada linha.
-// Cada linha tem dois arrays: outbound (IDA) e inbound (VOLTA).
+// cada linha tem dois arrays: outbound (IDA) e inbound (VOLTA).
 
 window.LINE_MAPPINGS = {
   "5": {
@@ -12,18 +11,44 @@ window.LINE_MAPPINGS = {
       [360, 0],
       [361, 0],
       [362, 0],
-      [363, 0]
+      [363, 0],
+      [364, 0],
+      [365,0],
+      [366,0],
+      [367,0],
+      [368,0],
+      [369,0],
+      [370,0],
+      [371,0],
+      [372,0],
+      [373,0],
+      [374,0],
+      [375,0],
+      [376,0],
+      [377,0]
       // adiciona as paragens de IDA
     ],
     inbound: [
-      [14, 0],
-      [13, 0],
-      [12, 0],
-      [11, 0],
-      [10, 0],
-      [9, 0]
+      [379, 0],
+      [380, 0],
+      [381, 0],
+      [382, 0],
+      [383, 0],
+      [384, 0],
+      [385, 0],
+      [386, 0],
+      [387, 0],
+      [388, 0],
+      [389, 0],
+      [390, 0],
+      [391, 0],
+      [392, 0],
+      [393, 0],
+      [394, 0],
+      [395, 0],
+      [396, 0]
       // adiciona as paragens de VOLTA
     ]
   }
-  // Outras linhas aqui...
+  // outras linhas aqui...
 };
